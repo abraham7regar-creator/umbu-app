44e7bd72d2d8b47fea3176e5cc3a3426c728cdef
Abraham Siregar <abraham7regar@student.gunadarma.ac.id>
2021-07-17 01:44:55 +0700

    Sync map with carousel flatlist

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { View, Text, FlatList, useWindowDimensions } from "react-native";
+import React, {useState, useEffect, useRef} from "react";
+import { View, FlatList, useWindowDimensions } from "react-native";
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import CustomMarker from "../../components/CustomMarker";
 import PostCarouselItem from "../../components/PostCarouselItem";
@@ -8,13 +8,42 @@ import places from '../../../assets/data/feed';
 
 const SearchResultsMaps = (props) => {
 
+  const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+
+  const flatlist = useRef();
+  const map = useRef();
+
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70})
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id)
+    }
+  })
 
   const width = useWindowDimensions().width;
-  const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatlist) {
+      return;
+    }
+    const index = places.findIndex(place => place.id === selectedPlaceId)
+    flatlist.current.scrollToIndex({index})
+
+    const selectedPlace = places[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    }
+    map.current.animateToRegion(region);
+  }, [selectedPlaceId])
 
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         style={{width: '100%', height: '100%'}}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
@@ -36,13 +65,16 @@ const SearchResultsMaps = (props) => {
 
       <View style={{position: 'absolute', bottom: 10}}>
         <FlatList
+          ref={flatlist}
           data={places}
-          renderItem={({item}) => <PostCarouselItem post={item}/>}
+          renderItem={({item}) => <PostCarouselItem post={item} />}
           horizontal
           showsHorizontalScrollIndicator={false}
           snapToInterval={width - 60}
-          snapToAlignment={'center'}
-          decelerationRate={'fast'}
+          snapToAlignment={"center"}
+          decelerationRate={"fast"}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
